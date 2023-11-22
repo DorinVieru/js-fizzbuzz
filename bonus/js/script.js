ab3c406ee3256612252fe40c0bbba192f320723d
DorinVieru <dorino.vieru@gmail.com>
2023-11-22 16:48:03 +0100

    [BONUS] Print on html all num. js with design css

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -5,24 +5,28 @@ const ul = document.querySelector("ul.list-group");
 for (let n = 1; n <= 100; n++){
 
     // MULTIPLI DI 3 E DI 5
-    if (n % 3 == 0 && n % 5 == 0){
-        element = `${n} "FizzBuss"`;
+    if (n % 3 == 0 && n % 5 == 0) {
+        element = `<li class="list-group-item acquamarine">${n} "FizzBuss"</li>`;
         console.log(element);
+        ul.innerHTML += element;
     }
     // MULTIPLI DI 5
     else if (n % 5 == 0) {
-        element = `${n} "Fizz"`;
+        element = `<li class="list-group-item green">${n} "Fizz"</li>`;
         console.log(element);
+        ul.innerHTML += element;
     }
     // MULTIPLI DI 3
-    else if (n % 3 == 0){
-        element = `${n} "Buss"`;
+    else if (n % 3 == 0) {
+        element = `<li class="list-group-item yellow">${n} "Buss"</li>`;
         console.log(element);
+        ul.innerHTML += element;
     }
     // NESSUN MULTIPLO
-    else{
-        let element = `${n}`;
+    else {
+        let element = `<li class="list-group-item">${n}</li>`;
         console.log(element);
+        ul.innerHTML += element;
     }
 
     
