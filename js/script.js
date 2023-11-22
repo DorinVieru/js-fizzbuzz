9779fb8c1a08d6bf7a3aefd6e956625a7c03f633
DorinVieru <dorino.vieru@gmail.com>
2023-11-22 16:38:25 +0100

    [UPDATE] Print in console.log all number

--- js/script.js
+++ js/script.js
@@ -3,7 +3,27 @@ const ul = document.querySelector("ul.list-group");
 
 // IMPOSTO IL CICLO DI NUMERAZIONE
 for (let n = 1; n <= 100; n++){
-    let element = `<li class="list-group-item">${n}</li>`;
-    ul.innerHTML += element;
-    console.log(n);
+
+    // MULTIPLI DI 3 E DI 5
+    if (n % 3 == 0 && n % 5 == 0){
+        element = `${n} "FizzBuss"`;
+        console.log(element);
+    }
+    // MULTIPLI DI 5
+    else if (n % 5 == 0) {
+        element = `${n} "Fizz"`;
+        console.log(element);
+    }
+    // MULTIPLI DI 3
+    else if (n % 3 == 0){
+        element = `${n} "Buss"`;
+        console.log(element);
+    }
+    // NESSUN MULTIPLO
+    else{
+        let element = `${n}`;
+        console.log(element);
+    }
+
+    
 }
